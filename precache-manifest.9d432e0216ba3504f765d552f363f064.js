715552aab91bbb0603811b747e81541a43865cdb
ijhajj <i.jhajj@gmail.com>
2020-05-15 14:20:22 +0530

    Updates

---
+++ precache-manifest.9d432e0216ba3504f765d552f363f064.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd0d0f05ceb33042b22d3aa1462600e5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "69e7ea90276617ac1691",
+    "url": "/monsters-rolodex/static/css/main.f5a3901e.chunk.css"
+  },
+  {
+    "revision": "ca96cb1d2607882b95b5",
+    "url": "/monsters-rolodex/static/js/2.9a317882.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.9a317882.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69e7ea90276617ac1691",
+    "url": "/monsters-rolodex/static/js/main.575bbfb4.chunk.js"
+  },
+  {
+    "revision": "60b3e6aee1ab568e197c",
+    "url": "/monsters-rolodex/static/js/runtime-main.ed829367.js"
+  }
+]);
